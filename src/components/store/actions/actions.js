6a2bb80fc628caf435ae4d64440a3b2c1e5869db
İhsan Yazarel <ihsanyazarel@hotmail.com>
2023-05-25 03:36:37 +0300

    update

--- src/components/store/actions/actions.js
+++ src/components/store/actions/actions.js
@@ -1,17 +1,11 @@
-export const ADD_MOVIE = "DELETE_MOVIE";
 export const BACK_TO_FIRST = "BACK_TO_FIRST";
 export const NEXT_MOVIE = "NEXT_MOVIE";
 export const PREVIOUS_MOVIE = "PREVIOUS_MOVIE";
-export const DELETE_MOVIE = "DELETE_MOVIE";
 export const ADD_LIST = "ADD_LIST";
 export const GO_TO_LAST = "GO_TO_LAST";
 export const DELETE_FAV = "DELETE_FAV";
 
 
-
-export const addMovie = (id) => {
-    return ({ type: ADD_MOVIE, payload: id });
-}
 
 export const backToFirst = () => {
     return ({ type: BACK_TO_FIRST });
@@ -25,9 +19,6 @@ export const previousMovie = () => {
     return ({ type: PREVIOUS_MOVIE});
 }
 
-export const deleteMovie = (movie) => {
-    return ({ type: DELETE_MOVIE, payload: movie });
-}
 export const listeyeEkle = (movie) => {
     return ({ type: ADD_LIST, payload: movie });
 }
